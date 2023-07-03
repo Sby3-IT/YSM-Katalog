9487f55c4131202c9da701d745d9642ff2a6147a
Sby3-IT <125420793+Sby3-IT@users.noreply.github.com>
2023-07-03 13:43:48 +0700

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,21 @@
-document.querySelectorAll('.btnDetail').forEach(item =>{
-    item.addEventListener('click',(e) => {
+document.querySelectorAll('.btnDetail').forEach(item => {
+    item.addEventListener('click', (e) => {
         let parent = e.target.parentNode.parentNode;
         let gambar = parent.querySelector('.card-img-top').src;
-        console.log('gambar: ${gambar}');
+        let judul = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi')? parent.querySelector('.deskripsi').innerHTML :
+        '<i>Tidak ada deskripsi barang</i>';
+
+       
+       let tombolModal = document.querySelector('.btnModal');
+       tombolModal.click();
+
+       document.querySelector('.modalTitle').innerHTML = judul;
+       let image = document.createElement('img');
+       image.src = gambar;
+       image.classList.add('w-50');
+       document.querySelector('.modalImage').innerHTML = '';
+       document.querySelector('.modalImage').appendChild(image);
+       document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
     })
 })
